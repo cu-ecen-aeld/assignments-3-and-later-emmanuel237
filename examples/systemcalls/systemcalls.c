47fc62d84e7fdddb9d810b02a407210900ee93b6
emmanuel237 <kiegaingemmanuel@gmail.com>
2024-04-25 08:56:48 +0200

    assignment3 completed

--- examples/systemcalls/systemcalls.c
+++ examples/systemcalls/systemcalls.c
@@ -1,5 +1,14 @@
 #include "systemcalls.h"
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/wait.h>
+#include <stdio.h>
+#include <sys/types.h>
+#include <sys/stat.h>
 
+#include <fcntl.h>
+#include <errno.h>
+#include <string.h>
 /**
  * @param cmd the command to execute with system()
  * @return true if the command in @param cmd was executed
@@ -16,8 +25,18 @@ bool do_system(const char *cmd)
  *   and return a boolean true if the system() call completed with success
  *   or false() if it returned a failure
 */
+ bool retval = true;
+if( cmd == NULL )
+{
+    retval = false;
+}
+    int system_retval = system(cmd);
+    if( system_retval == -1 || system_retval == 2)
+    {
+        retval = false;
+    }
 
-    return true;
+    return retval;
 }
 
 /**
@@ -45,10 +64,7 @@ bool do_exec(int count, ...)
         command[i] = va_arg(args, char *);
     }
     command[count] = NULL;
-    // this line is to avoid a compile warning before your implementation is complete
-    // and may be removed
-    command[count] = command[count];
-
+      command[count] =   command[count] ;
 /*
  * TODO:
  *   Execute a system command by calling fork, execv(),
@@ -58,10 +74,37 @@ bool do_exec(int count, ...)
  *   as second argument to the execv() command.
  *
 */
-
     va_end(args);
 
-    return true;
+     if(count < 3 ||  command[0][0] != '/' || command[2][0] != '/')
+        return false;
+  
+
+    int status;
+    pid_t pid = fork();
+    
+    if( pid == -1 )
+    {
+        return false;
+    }
+    else if(pid == 0)
+    {
+        execv(command[0], &command[1]);
+        exit(-1);
+    }
+
+    if(waitpid(pid, &status, 0) == -1)
+    {
+        return false;
+    }
+    else if( WIFEXITED(status) )
+    {
+        if( status == 0 )
+                return true;
+        else
+                return false;
+    }
+    return false;
 }
 
 /**
@@ -80,11 +123,12 @@ bool do_exec_redirect(const char *outputfile, int count, ...)
         command[i] = va_arg(args, char *);
     }
     command[count] = NULL;
-    // this line is to avoid a compile warning before your implementation is complete
-    // and may be removed
-    command[count] = command[count];
 
+    va_end(args);
 
+    if(count < 3 ||  command[0][0] != '/' || command[2][0] != '/')
+        return false;
+  
 /*
  * TODO
  *   Call execv, but first using https://stackoverflow.com/a/13784315/1446624 as a refernce,
@@ -93,7 +137,49 @@ bool do_exec_redirect(const char *outputfile, int count, ...)
  *
 */
 
-    va_end(args);
 
-    return true;
+    int fd = open(outputfile, O_WRONLY|O_TRUNC|O_CREAT, 0644);
+
+    if(fd <0)
+    {
+        perror("open");
+        return false;
+    }
+
+    int status;
+    fflush(stdout) ;
+    pid_t pid = fork();
+    
+    if( pid == -1 )
+    {
+        close(fd);
+        return false;
+    }
+    else if(pid == 0)
+    {
+        if(dup2(fd, 1) < 0 ){
+            perror("dup2");
+            close(fd);
+            exit(-1);
+        }
+        execv(command[0], &command[1]);
+        exit(-1);
+    }
+
+    if(waitpid(pid, &status, 0) == -1)
+    {
+        close(fd);
+        return false;
+    }
+    else if( WIFEXITED(status) )
+    {
+        close(fd);
+        if( status == 0 )
+                return true;
+        else
+                return false;
+    }
+
+    close(fd);
+    return false;
 }
